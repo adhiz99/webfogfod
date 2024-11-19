ddec94fd0ba8ee3ba9998bf26571812c77d34fae
bimosptr1 <bimosaputro6@gmail.com>
2024-11-19 10:42:23 +0700

    penambahan offset parameter

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ flow of goods and flow of documents
 Auth Basic Auth
 
 example
-Api  : http://localhost:8080/api/fogfod/tfog-h-int?keyword=&limit=10
+Api  : http://localhost:8080/api/fogfod/tfog-h-int?keyword=&period_start=&period_end&limit=10&offset=0
 keyword untuk search no_pkk_inaportnet, no_pkk, vessel_name, name_branch, name_organization
 
-Api 2 : http://localhost:8080/api/fogfod/tfog-d-int?id_ppkb=57456&limit=10
+Api 2 : http://localhost:8080/api/fogfod/tfog-d-int?id_ppkb=57456&limit=10&offset=0
 
-Api 3 : http://localhost:8080/api/fogfod/tfod-int?id_pkk=321838&no_pmh=BM61880011461024&limit=10
+Api 3 : http://localhost:8080/api/fogfod/tfod-int?id_pkk=321838&no_pmh=BM61880011461024&limit=10&offset=0
--- src/services/fogfod/flow.service.js
+++ src/services/fogfod/flow.service.js
@@ -1,13 +1,18 @@
 const connectKudu = require("../../config/database.config");
 
 exports.get_tfog_h_int = async (payload) => {
-  const { keyword = '', period_start = '', period_end = '', limit = '' } = payload
+  const { keyword = '', period_start = '', period_end = '', limit = '', offset = '' } = payload
 
   let batas = `LIMIT 25`
+  let offsets = `OFFSET 0`
   let periode = ``
 
   if (limit) {
     batas = `LIMIT ${limit}`
+  }
+
+  if (offset) {
+    offsets = `OFFSET ${offset}`
   }
 
   if (period_start && period_end) {
@@ -24,10 +29,12 @@ exports.get_tfog_h_int = async (payload) => {
               LOWER(vessel_name)  LIKE LOWER('%${keyword}%') OR
               LOWER(name_branch) LIKE LOWER('%${keyword}%') OR
               LOWER(name_organization) LIKE LOWER('%${keyword}%'))
-              ${periode}
+              ${periode} ORDER BY no_pkk_inaportnet DESC
           ${batas}
+          ${offsets}
           ;
           `
+          console.log(query);
           
         const data = await kudu.query(query);
         
@@ -39,25 +46,31 @@ exports.get_tfog_h_int = async (payload) => {
 };
 
 exports.get_tfog_d_init = async (payload) => {
-  const { id_ppkb = '', limit = '' } = payload
+  const { id_ppkb = '', limit = '', offset = '' } = payload
 
     let batas = `LIMIT 25`
-
+    let offsets = `OFFSET 0`
     let id_ppkbs = ``
-    if (id_ppkb) {
-      id_ppkbs = `AND id_ppkb = ${id_ppkb}`
-    }
 
     if (limit) {
       batas = `LIMIT ${limit}`
+    }
+
+    if (offset) {
+      offsets = `OFFSET ${offset}`
+    }
+
+    if (id_ppkb) {
+      id_ppkbs = `AND id_ppkb = ${id_ppkb}`
     }
 
     try {
         const kudu = await connectKudu();
         const query = `
           SELECT * FROM t3_mntrfog_dev.t_fog_d_int
-          WHERE 1=1 ${id_ppkbs}
+          WHERE 1=1 ${id_ppkbs} ORDER BY id_ppkb DESC
           ${batas}
+          ${offsets}
           ;
           `
         const data = await kudu.query(query);
@@ -70,17 +83,22 @@ exports.get_tfog_d_init = async (payload) => {
 };
 
 exports.get_tfod_int = async (payload) => {
-  const { id_pkk = '', no_pmh = '', limit = '' } = payload
+  const { id_pkk = '', no_pmh = '', limit = '', offset = '' } = payload
 
   let batas = `LIMIT 25`
-
+  let offsets = `OFFSET 0`
   let id_pkks = ``
-  if (id_pkk) {
-    id_pkks = `AND id_pkk = ${id_pkk}`
-  }
 
   if (limit) {
     batas = `LIMIT ${limit}`
+  }
+
+  if (offset) {
+    offsets = `OFFSET ${offset}`
+  }
+
+  if (id_pkk) {
+    id_pkks = `AND id_pkk = ${id_pkk}`
   }
 
     try {
@@ -89,8 +107,9 @@ exports.get_tfod_int = async (payload) => {
           SELECT * FROM t3_mntrfog_dev.t_fod_int
           WHERE 1=1
               AND LOWER(no_pmh) LIKE LOWER('%${no_pmh}%')
-              ${id_pkks}
+              ${id_pkks} ORDER BY id_pkk DESC
           ${batas}
+          ${offsets}
           ;
           `
         const data = await kudu.query(query);
